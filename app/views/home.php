d879894fd6ccd6d3dd80788f662cc76d9db5c825
Francisco Barba <barbapogonzafrancisco@gmail.com>
2025-12-25 20:42:45 -0300

    creacion home.php

---
+++ app/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/CSS/styles.css">
+    <title>Home</title>
+</head>
+<body>
+    <header class="head">
+        <h1>AXIOM</h1>
+        <h2>Bienvenido</h2>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Front/Estilos/styles.css">
-    <title>Home</title>
-</head>
-<body>
-    <h1>Biblioteca</h1>
-    <nav class="nav-bar">
-        <ul>
-            <li class="nav-item">item 1</li>
-            <li class="nav-item">item 2</li>
-            <li class="nav-item">item 3</li>
-            <li class="nav-item">item 4</li>
-        </ul>
-    </nav>
-    <header>
+<?php 
 
-    </header>
-    <main>
+require_once __DIR__ .'/../app/views/home.php';
 
-    </main>
-    <footer>
-        Suburbana Code
-    </footer>
-    <script src="./theme.js"></script>
-</body>
-</html>
+?>
